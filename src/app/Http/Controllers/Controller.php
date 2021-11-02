2fb73ee87dcb7676ea6b6b94ed985993f3eab7f7
Vinicius Matteus <vmatteus@gmail.com>
2021-11-02 06:01:35 -0300

    add request defaults into base controller

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -10,4 +10,19 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function requestOk($data) {
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+
+    public function requestError(\Exception $e) {
+        return response()->json([
+            'error' => [
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ]
+        ]);
+    }
 }
