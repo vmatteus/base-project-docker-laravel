10613cfa5671f1732fc0d86a97fd789d425dd92b
Vinicius Matteus <vmatteus@gmail.com>
2021-11-03 12:35:47 -0300

    modify database config

--- src/config/database.php
+++ src/config/database.php
@@ -37,8 +37,8 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'url' => null,
+            'database' => database_path(env('DB_DATABASE')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
