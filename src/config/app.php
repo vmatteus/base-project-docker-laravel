e7a95f052d25a132f0e5e1d74d3d012fd7f6cc94
Vinícius Matteus <vinicius.matteus@appmax.com.br>
2022-03-28 17:29:37 -0300

    adicionei o novo pacote e configurei o envio para fila

--- src/config/app.php
+++ src/config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        NeedleProject\LaravelRabbitMq\Providers\ServiceProvider::class,
+
     ],
 
     /*
---
+++ src/config/laravel_rabbitmq.php
@@ -0,0 +1,41 @@
+<?php
+/** For documentation, see https://github.com/needle-project/laravel-rabbitmq */
+return [
+    'connections' => [
+        'connectionA' => [
+            'hostname' => '172.18.0.1',
+            'port'     => '5672',
+            'username' => 'rabbitmq',
+            'password' => 'rabbitmq',
+        ],
+    ],
+    'exchanges' => [
+        'exchangeA' => [
+            'connection' => 'connectionA',
+			'name' => 'amq.direct',
+			'attributes' => [
+				'exchange_type' => 'topic'
+			]
+        ]
+	],
+    'queues' => [
+        'queueB' => [
+            'connection' => 'connectionA',
+            'name' => 'queueB',
+			'attributes' => [
+				'bind' => [
+                    ['exchange' => 'foo_bar', 'routing_key' => '*']
+                ]
+			]
+        ]
+    ],
+    'publishers' => [
+        'aPublisherName' => 'exchangeA'
+    ],
+    'consumers' => [
+        'aConsumerName' => [
+            'queue' => 'queueB',
+            'message_processor' => \NeedleProject\LaravelRabbitMq\Processor\CliOutputProcessor::class
+        ]
+    ]
+];
